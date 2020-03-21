4d1e84e7d9f6c7305641a213e538a1d017c28c9c
Magnus Nyberg <magnus@nexter.se>
2020-03-21 23:11:29 +0100

    custom: Set visual bell

--- custom.el
+++ custom.el
@@ -3,9 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
-   (quote
-    ((bug-reference-bug-regexp . "#\\(?2:[[:digit:]]+\\)")))))
+ '(visible-bell t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
