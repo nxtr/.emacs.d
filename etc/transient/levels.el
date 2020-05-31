c5a6c86bb83d5eba69a6d404babe2b247eaa30cc
Magnus Nyberg <magnus@nexter.se>
2020-05-31 15:15:20 +0200

    transient,magit: Add commit --date, file-dispatch etc

---
+++ etc/transient/levels.el
@@ -0,0 +1,13 @@
+((magit-commit
+  (magit-commit-autofixup . 1)
+  (magit-commit-reshelve . 1)
+  (magit:--gpg-sign . 1)
+  (transient:magit-commit:--date= . 1))
+ (magit-file-dispatch
+  (magit-file-checkout . 1)
+  (magit-file-untrack . 1)
+  (magit-file-delete . 1)
+  (t . 1)
+  (magit-file-rename . 1))
+ (magit-rebase
+  (magit-reshelve-since . 2)))
